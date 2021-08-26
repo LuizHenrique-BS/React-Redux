293d32a3662d00527a068ca6d03a2cd1211e50e5
Luiz Barros <luiz.henrique.lhbs@gmaill.com>
2021-08-26 15:45:30 -0400

    Componente Card #1

--- src/components/basicos/Aleatorio.jsx
+++ src/components/basicos/Aleatorio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Aleatorio(props) {
-  const { min, max } = props; // destruction
+  const { min, max } = props; // destructuring
   return (
     <div>
       <p>
