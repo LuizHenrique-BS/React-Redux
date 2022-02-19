effed36ecaab49b185bcec7a86486af5dc8ee174
Luiz Barros <luiz.henrique.lhbs@gmail.com>
2022-02-19 14:04:24 -0400

    Componente com estado

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 export default (props) => {
   const cb = props.quandoClicar;
+  const min = 50
+  const max = 70
+  const gerarIdade = () => parseInt(Math.random() * (max-min) + min)
+  const gerarNerd = () => Math.random() > 0.5
   return (
     <div>
       <div>Filho</div>
       <button
         onClick={function (e) { // funcao anonima
-          cb("João", 53, true); // equivalente a "_ => cb('João, 53, true')"
+          cb("João", gerarIdade, gerarNerd); // equivalente a "_ => cb('João, 53, true')"
         }}
       >
         Fornecer Informações
--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default (props) => {
-  let nome = '?'
-  let idade = 0
-  let nerd = false
-  function fornecerInformacoes(nomeParam, idadeParam, nerdParam) {
-    nome = nomeParam
-    idade = idadeParam
-    nerd = nerdParam
-    
-    console.log(nomeParam, idadeParam, nerdParam);
+  // useState retorna um array de 2 pos, a primeira eh o elemento e a segunda uma funcao pra altera-lo
+  const [nome, setNome] = useState("?");
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
+
+
+  function fornecerInformacoes(nome, idade, nerd) {
+    setNome(nome)
+    setIdade(idade)
+    setNerd(nerd)
   }
   return (
     <div>
@@ -18,8 +19,10 @@ export default (props) => {
         Pai
         <br />
         <span>{nome} </span>
-        <span><strong>{idade} </strong></span>
-        <span>{nerd ? 'Verdadeiro' : 'False'}</span>
+        <span>
+          <strong>{idade} </strong>
+        </span>
+        <span>{nerd ? "Verdadeiro" : "False"}</span>
       </div>
       <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
     </div>
